3e64df22ba07869836c90495884c9cc62bb280fd
salvadorsgm <111320779+salvadorsgm@users.noreply.github.com>
2022-08-17 15:19:13 -0500

    Add files via upload

---
+++ reccurse3.3act/src/reccurse3/pkg3act/Reccurse33act.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reccurse3.pkg3act;
+
+/**
+ *
+ * @author Lenovo Corei7
+ */
+public class Reccurse33act {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                new frmprincipal().setVisible(true);
+                
+
+    }
+    
+}
