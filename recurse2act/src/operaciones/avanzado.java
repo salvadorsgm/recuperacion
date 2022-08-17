7d802e3a7e2d4604576170d2bfd40bedd8258b9e
salvadorsgm <111320779+salvadorsgm@users.noreply.github.com>
2022-08-17 15:23:25 -0500

    Add files via upload

---
+++ recurse2act/src/operaciones/avanzado.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operaciones;
+
+/**
+ *
+ * @author Lenovo Corei7
+ */
+public class avanzado {
+    public double potencia(int base,int elevacion){
+        return Math.pow(base ,elevacion);
+    }
+}
---
+++ recurse2act/src/operaciones/basicas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operaciones;
+
+/**
+ *
+ * @author Lenovo Corei7
+ */
+public class basicas{ 
+    
+        public int Sumar(int num1, int num2){
+            return(num1 + num2 );
+            
+        }
+        public int Resta(int a, int b){
+            return(a-b);
+        }
+    }
